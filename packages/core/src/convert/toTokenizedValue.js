0e1452df01998096910c0eadf38308891be9a48a
Erik Isidore <ericssonico@protonmail.ch>
2023-06-08 17:40:09 -0300

    Solid: WIP: Fix toTokenizedValue converter

--- packages/core/src/convert/toTokenizedValue.js
+++ packages/core/src/convert/toTokenizedValue.js
@@ -1,12 +1,54 @@
 import { toTailDashed } from './toTailDashed.js'
 
-/** Returns a declaration value with transformed token values. */
+export const toTokenizedValue = (value, prefix, scale) => {
+	const regex = new RegExp([
+		'([+-])?((?:\\d+(?:\\.\\d*)?|\\.\\d+)',
+		'(?:[Ee][+-]?\\d+)?)?(\\$|--)([$.\\w-]+)'
+	].join(''), 'g')
+
+	return value.replace(regex,
+		($0, direction, multiplier, separator, token) => {
+			console.log({
+				$0,
+				direction,
+				multiplier,
+				separator,
+				token
+			})
+
+			// This is peak readable code, congrats stitches' devs
+			return separator == "$" == !!multiplier ? $0
+				: (
+					direction || separator == '--'
+						? 'calc('
+					: ''
+				) + (
+					'var(--' + (
+						separator === '$'
+							? toTailDashed(prefix) + (
+								!token.includes('$')
+									? toTailDashed(scale)
+								: ''
+							) + token.replace(/\.|\$/g, '-')
+						: token
+					) + ')' + (
+						direction || separator == '--'
+							? '*' + (
+								direction || ''
+							) + (
+								multiplier || '1'
+							) + ')'
+						: ''
+					)
+				)
+		})
+}
+
+/*
+/** Returns a declaration value with transformed token values
 export const toTokenizedValue = (
-	/** @type {string} */
 	value,
-	/** @type {string} */
 	prefix,
-	/** @type {string} */
 	scale,
 ) => value.replace(
 	/([+-])?((?:\d+(?:\.\d*)?|\.\d+)(?:[Ee][+-]?\d+)?)?(\$|--)([$\w-]+)/g,
@@ -38,3 +80,4 @@ export const toTokenizedValue = (
 		)
 	),
 )
+*/
